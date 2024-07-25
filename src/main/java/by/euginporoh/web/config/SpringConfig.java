52952515537afa92259508aa39e45fa5905b66b8
Eugene Prohorenkov <oneraidzenya@gmail.com>
2024-07-25 17:29:03 +0300

    Update Spring config, Create sql Create table, create hibernate.properties file.

--- src/main/java/by/euginporoh/web/config/SpringConfig.java
+++ src/main/java/by/euginporoh/web/config/SpringConfig.java
@@ -1,6 +1,7 @@
 package by.euginporoh.web.config;
 
 import java.util.Objects;
+import java.util.Properties;
 
 import javax.sql.DataSource;
 
@@ -11,8 +12,14 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -24,14 +31,17 @@ import org.thymeleaf.spring5.view.ThymeleafViewResolver;
 @ComponentScan("by.euginporoh.web")
 @EnableWebMvc
 @PropertySource("classpath:database.properties")
+@EnableTransactionManagement
+@EnableJpaRepositories("by.euginporoh.web.repositories")
 public class SpringConfig implements WebMvcConfigurer {
 	private final ApplicationContext applicationContext;
-	private final Environment environment;
+	private final Environment env;
 
     @Autowired
-    public SpringConfig(ApplicationContext applicationContext, Environment environment) {
-        this.applicationContext = applicationContext;
-		this.environment = environment;
+    public SpringConfig(ApplicationContext applicationContext, Environment env) {
+        
+    	this.applicationContext = applicationContext;
+		this.env = env;
     }
 
     @Bean
@@ -64,16 +74,45 @@ public class SpringConfig implements WebMvcConfigurer {
     public DataSource dataSource() {
     	DriverManagerDataSource dataSource = new DriverManagerDataSource();
     	
-    	dataSource.setDriverClassName(Objects.requireNonNull(environment.getProperty("driver")));
-    	dataSource.setUrl(environment.getProperty("url"));
-    	dataSource.setUsername(environment.getProperty("username_value"));
-    	dataSource.setPassword(environment.getProperty("password"));
+    	dataSource.setDriverClassName(env.getRequiredProperty("hibernate.driver_class"));
+    	dataSource.setUrl(env.getRequiredProperty("hibernate.connection.url"));
+    	dataSource.setUsername(env.getRequiredProperty("hibernate.connection.username"));
+    	dataSource.setPassword(env.getRequiredProperty("hibernate.connection.password"));
     	
 		return dataSource;
     }
     
+//    @Bean
+//    public JdbcTemplate jdbcTemplate() {
+//    	return new JdbcTemplate(dataSource());
+//    }
+    
+    private Properties hibernateProperties() {
+    	Properties properties = new Properties();
+    	properties.put("hibernate.dialect", env.getRequiredProperty("hibernate.dialect"));
+    	properties.put("hibernate.show_sql", env.getRequiredProperty("hibernate.show_sql"));
+    	
+    	return properties;
+    }
+    
     @Bean
-    public JdbcTemplate jdbcTemplate() {
-    	return new JdbcTemplate(dataSource());
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+    	final LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
+    	em.setDataSource(dataSource());
+    	em.setPackagesToScan("by.euginporoh.web.models");
+    	
+    	final HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+    	em.setJpaVendorAdapter(vendorAdapter);
+    	em.setJpaProperties(hibernateProperties());
+    	
+    	return em;
+    }
+    
+    @Bean
+    public PlatformTransactionManager transactionManager() {
+    	JpaTransactionManager transactionManager = new JpaTransactionManager();
+    	transactionManager.setEntityManagerFactory(entityManagerFactory().getObject());
+    	
+    	return transactionManager;
     }
 }
